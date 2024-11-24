5085bea25fc3587387da587a40b706c3fcba6784
Jarkko Stålnacke <136805206+jlostaln@users.noreply.github.com>
2024-11-24 16:40:35 +0200

    small updates to index.js

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
