e021886533527816205bbdf2f8d9961326785f84
Jarkko Stålnacke <136805206+jlostaln@users.noreply.github.com>
2024-11-29 11:47:07 +0200

    controllers added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
+// todo: tehtävä 4.2 kesken: ainakin app.js
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
@@ -14,24 +16,8 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
